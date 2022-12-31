e7d7d9ce0eb4b834de4b964950d97962353db45a
Awang <m15950730938@gmail.com>
2022-12-31 23:45:42 +0800

    test 1

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base: "/vue-demo/",
+  build: {
+    outDir: "docs"
   }
 })
